b12db9f721e32e8ad4b90358431e741742130ab8
Matthew Enterline <matthew.j.enterline@gmail.com>
2013-12-27 23:51:53 -0500

    Good files

---
+++ Control.java
@@ -0,0 +1,84 @@
+import java.lang.Math;
+public class Control {
+    
+	double[] AnsArr;
+	static double Ans;
+	static double Error;
+    static double[] inputArr = new double[4];
+    static boolean doAdd = false;
+    static boolean doMinus = false;
+    static boolean doTimes = false;
+    static boolean doDiv = false;
+    
+    //Checking if error has been hit
+    static  boolean errStatus = false;
+    
+    //Addition
+	public static double[] Add(double[] inp) {
+        double num1 = inp[0]; double err1 = inp[1]; double num2 = inp[2]; double err2 = inp[3];
+		double ans = num1 + num2;
+		double err = err1 + err2;
+		Ans = num1 + num2;
+		Error = err1 + err2;
+		double[] AnsArr = {ans, err};
+		return AnsArr;
+	}
+	//Subtraction
+	public static double[] Subtract(double[] inp) {
+		double num1 = inp[0]; double err1 = inp[1]; double num2 = inp[2]; double err2 = inp[3];
+		double ans = num1 - num2;
+		double err = err1 + err2;
+		Ans = ans;
+		Error = err;
+		double[] AnsArr = {ans, err};
+		return AnsArr;
+	}
+	//Multiply
+	public static double[] Multiply(double[] inp) {
+		double num1 = inp[0]; double err1 = inp[1]; double num2 = inp[2]; double err2 = inp[3];
+		double ans = num1*num2;
+		double err = ans*Math.pow(Math.pow(err1/num1, 2) + Math.pow(err2/num2, 2), 1/2);
+		Ans = ans;
+		Error = err;
+		double[] AnsArr = {ans, err};
+		return AnsArr;
+	}
+	//Division
+	public static double[] Divide(double[] inp) {
+		double num1 = inp[0]; double err1 = inp[1]; double num2 = inp[2]; double err2 = inp[3];
+		double ans = num1/num2;
+		double err = ans*Math.pow(Math.pow(err1/num1, 2) + Math.pow(err2/num2, 2), 1/2);
+		Ans = ans;
+		Error = err;
+		double[] AnsArr = {ans, err};
+		return AnsArr;
+	}
+	
+	//Sin
+	
+	//Cos
+		
+	//Tan
+	
+	//Log
+	
+	//
+	
+	//Find which operation to perform, and do it
+	public static double[] doOperation(double[] inputArr) {
+		double[] ans = {0,0};
+		if (doAdd) {
+			ans = Add(inputArr);	
+		}
+		if (doMinus) {
+			ans = Subtract(inputArr);
+		}
+		if (doTimes) {
+			ans= Multiply(inputArr);
+		}
+		if (doDiv) {
+			ans = Divide(inputArr);
+		}
+		return ans;
+	}
+}
---
+++ MyTextField.java
@@ -0,0 +1,11 @@
+import javax.swing.JTextField;
+
+public class MyTextField extends JTextField {
+
+    public void append(String txt) {
+        String current = this.getText();
+        this.setText(current + txt);
+        
+    }
+
+}
